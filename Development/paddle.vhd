d1a271d921671a4d2b656ac3c39816ebfb2167f4
Ty <ty.h.ahrens@gmail.com>
2025-04-08 10:07:59 -0500

    Added the incrementing and decrementing of the position for the paddle. Also added a paddleWidth variable to make the position update often

--- Development/paddle.vhd
+++ Development/paddle.vhd
@@ -4,21 +4,36 @@
 
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all; 
 
 
 entity paddle is
-    generic(pos : integer := 0);
+    generic(pos : unsigned_integer := 0;     -- used for keeping the position of the leftmost pixel
+            paddleWidth : std_logic_vector(7 downto 0) := "00011001");  -- paddle size is 25 pixels long  
 
+    -- port IN: used for two different channels form rotary encoder
+    -- port OUT: used for the left and right pixel positions to display on the VGA port
     port(ChannelA, ChannelB : IN std_logic;
-         middlePixel, rightPixel, leftPixel : OUT std_logic_vector(7 downto 0));
-         
+         rightPixel, leftPixel : OUT std_logic_vector(7 downto 0));
+
 end paddle;
 
-architecture behavior of paddle is 
+architecture behavior of paddle is  
+
+    --signal used to convert the position to a binary value instead of an integer for the display
+    signal conv_pos : std_logic_vector(to_unsigned(pos, 7));
 
 begin
-    movePaddle : process(ChannelA){
+    movePaddle : process(ChannelA)
+    begin
+        changeLocation : process(ChannelB)
+        begin
+            if (ChannelB = '1') then 
+                pos <= pos + 1;
+            elsif (ChannelB = '0') then 
+                pos <= pos - 1; 
+            end if;
+        end process changeLocation;
 
-    }
-
+    end process movePaddle;
 end behavior;
