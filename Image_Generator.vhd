c69d7b1d2830715658247f2d48df9a76f73b9922
bhudson0525 <blakehhudson@icloud.com>
2025-04-14 14:08:51 -0500

    4th

--- Image_Generator.vhd
+++ Image_Generator.vhd
@@ -119,7 +119,7 @@ architecture behavior of hw_image_generator is
 	 
 
 begin	 	 
-    process(disp_ena, row, column, encoder_value, ball_posL, ball_posT)
+    process(disp_ena, row, column, encoder_value, ball_left, ball_top)
         variable paddle_posL : integer;
         variable paddle_posR : integer;
 
