da9b9a326b3b73607985190c1e81329c1e065fe2
Ty <ty.h.ahrens@gmail.com>
2025-04-14 11:53:15 -0500

    Imported Ty's code for the generation of the blocks.

--- Image_Generator.vhd
+++ Image_Generator.vhd
@@ -1,3 +1,7 @@
+--Name: Ty Ahrens 
+--Date: 4/13/2025
+--Purpose: Generate an image for the blocks, paddle, and border
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -5,156 +9,147 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity hw_image_generator is
     port (
-        disp_ena : in  STD_LOGIC;
-        row      : in  INTEGER;
-        column   : in  INTEGER;
-		  RE_Val	  : in  integer;
-        red      : out STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
-        green    : out STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
-        blue     : out STD_LOGIC_VECTOR(7 downto 0) := (others => '0')
+        disp_ena        : in  STD_LOGIC;
+        row             : in  INTEGER;
+        column          : in  INTEGER;
+	    encoder_value   : in  INTEGER;
+        red             : out STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
+        green           : out STD_LOGIC_VECTOR(7 downto 0) := (others => '0');
+        blue            : out STD_LOGIC_VECTOR(7 downto 0) := (others => '0')
     );
 end hw_image_generator;
 
 architecture behavior of hw_image_generator is
 
-	 constant paddle_top    : integer := 450;
-    constant paddle_bottom : integer := 460;
+    constant block_start_x : integer := 25;
+    constant block_start_y : integer := 100;
+    constant block_width   : integer := 35;
+    constant block_height  : integer := 10;
+    constant block_width_spacing : integer := 7;
+    constant block_height_spacing : integer := 5; 
+
+
+	constant paddle_top     : integer := 450;
+    constant paddle_bottom  : integer := 460;
     constant paddle_left    : integer := 290;
-    constant paddle_right : integer := 350;
-	 constant paddle_width : integer := 60;
+    constant paddle_right   : integer := 350;
+	constant paddle_width   : integer := 60;
 
-    constant BORDER_TOP   : integer := 10;
-    constant BORDER_LEFT  : integer := 9;
-    constant BORDER_RIGHT : integer := 629;
+    constant border_width  : integer := 15;
+    constant BORDER_TOP   : integer := 0 + border_width; 
+    constant BORDER_LEFT  : integer := -2 + border_width;
+    constant BORDER_RIGHT : integer := 635 - border_width;
 
-    constant row1_top    : integer := 15;
-    constant row1_bottom : integer := 30;
-    constant row2_top    : integer := 34;
-    constant row2_bottom : integer := 49;
+    constant row1_top    : integer := block_start_y;
+    constant row1_bottom : integer := row1_top + block_height;
+    constant row2_top    : integer := row1_bottom + block_height_spacing;
+    constant row2_bottom : integer := row2_top + block_height;
+    constant row3_top    : integer := row2_bottom + block_height_spacing;
+    constant row3_bottom : integer := row3_top + block_height;
+    constant row4_top    : integer := row3_bottom + block_height_spacing;
+    constant row4_bottom : integer := row4_top + block_height;
+    constant row5_top    : integer := row4_bottom + block_height_spacing;
+    constant row5_bottom : integer := row5_top + block_height;
+    constant row6_top    : integer := row5_bottom + block_height_spacing;
+    constant row6_bottom : integer := row6_top + block_height;
+    constant row7_top    : integer := row6_bottom + block_height_spacing;
+    constant row7_bottom : integer := row7_top + block_height;
+    constant row8_top    : integer := row7_bottom + block_height_spacing;
+    constant row8_bottom : integer := row8_top + block_height;
 
-    constant column1_left   : integer := 13;
-    constant column1_right  : integer := 53;
-    constant column2_left   : integer := 57;
-    constant column2_right  : integer := 97;
-    constant column3_left   : integer := 101;
-    constant column3_right  : integer := 141;
-    constant column4_left   : integer := 145;
-    constant column4_right  : integer := 185;
-    constant column5_left   : integer := 189;
-    constant column5_right  : integer := 229;
-    constant column6_left   : integer := 233;
-    constant column6_right  : integer := 273;
-    constant column7_left   : integer := 277;
-    constant column7_right  : integer := 317;
-    constant column8_left   : integer := 321;
-    constant column8_right  : integer := 361;
-    constant column9_left   : integer := 365;
-    constant column9_right  : integer := 405;
-    constant column10_left  : integer := 409;
-    constant column10_right : integer := 449;
-    constant column11_left  : integer := 453;
-    constant column11_right : integer := 493;
-    constant column12_left  : integer := 497;
-    constant column12_right : integer := 537;
-    constant column13_left  : integer := 541;
-    constant column13_right : integer := 581;
-    constant column14_left  : integer := 585;
-    constant column14_right : integer := 625;
+    -- Row array constants for FOR loop
+    type row_array is array(0 to 7) of integer;
+    constant row_tops : row_array := (
+        row1_bottom, row2_bottom, row3_bottom, row4_bottom,
+        row5_bottom, row6_bottom, row7_bottom, row8_bottom
+    );
+    constant row_bottoms : row_array := (
+        row1_bottom, row2_bottom, row3_bottom, row4_bottom,
+        row5_bottom, row6_bottom, row7_bottom, row8_bottom
+    );
+
+    constant column1_left   : integer := block_start_x;
+    constant column1_right  : integer := column1_left + block_width;
+    constant column2_left   : integer := column1_right + block_width_spacing;
+    constant column2_right  : integer := column2_left + block_width;
+    constant column3_left   : integer := column2_right + block_width_spacing;
+    constant column3_right  : integer := column3_left + block_width;
+    constant column4_left   : integer := column3_right + block_width_spacing;
+    constant column4_right  : integer := column4_left + block_width;
+    constant column5_left   : integer := column4_right + block_width_spacing;
+    constant column5_right  : integer := column5_left + block_width;
+    constant column6_left   : integer := column5_right + block_width_spacing;
+    constant column6_right  : integer := column6_left + block_width;
+    constant column7_left   : integer := column6_right + block_width_spacing;
+    constant column7_right  : integer := column7_left + block_width;
+    constant column8_left   : integer := column7_right + block_width_spacing;
+    constant column8_right  : integer := column8_left + block_width;
+    constant column9_left   : integer := column8_right + block_width_spacing;
+    constant column9_right  : integer := column9_left + block_width;
+    constant column10_left  : integer := column9_right + block_width_spacing;
+    constant column10_right : integer := column10_left + block_width;
+    constant column11_left  : integer := column10_right + block_width_spacing;
+    constant column11_right : integer := column11_left + block_width;
+    constant column12_left  : integer := column11_right + block_width_spacing;
+    constant column12_right : integer := column12_left + block_width;
+    constant column13_left  : integer := column12_right + block_width_spacing;
+    constant column13_right : integer := column13_left + block_width;
+    constant column14_left  : integer := column13_right + block_width_spacing;
+    constant column14_right : integer := column14_left + block_width;
+
+    -- Column arrary constants for FOR loop
+    type column_array is array(0 to 13) of integer;
+    constant column_lefts : column_array := (
+        column1_left, column2_left, column3_left, column4_left,
+        column5_left, column6_left, column7_left, column8_left,
+        column9_left, column10_left, column11_left, column12_left,
+        column13_left, column14_left
+    );
+    constant column_rights : column_array := (
+        column1_right, column2_right, column3_right, column4_right,
+        column5_right, column6_right, column7_right, column8_right,
+        column9_right, column10_right, column11_right, column12_right,
+        column13_right, column14_right
+        );
 	 
 
-begin
-	 	 
-    process(disp_ena, row, column, RE_Val)
-		variable paddle_posL : integer;
-		variable paddle_posR : integer;
+begin	 	 
+    process(disp_ena, row, column, encoder_value, fib1, fib2)
+        variable paddle_posL : integer;
+        variable paddle_posR : integer;
     begin
-        -- Default black background
-        red   <= (others => '0');
-        green <= (others => '0');
-        blue  <= (others => '0');
-		  
-		  	paddle_posL := (paddle_Left + RE_Val);
-			paddle_posR := (paddle_posL + paddle_width);
-			
-			
-			if row >= paddle_top and row <= paddle_bottom and column >= paddle_posL and column <= paddle_posR then
-           red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-			 end if;
+        -- Default color to black
+        red   <= X"00";
+        green <= X"00";
+        blue  <= X"00"; 
 
- 
-        -- First row blocks
-        if row >= row1_top and row <= row1_bottom and column >= column1_left and column <= column1_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row1_top and row <= row1_bottom and column >= column2_left and column <= column2_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row1_top and row <= row1_bottom and column >= column3_left and column <= column3_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row1_top and row <= row1_bottom and column >= column4_left and column <= column4_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row1_top and row <= row1_bottom and column >= column5_left and column <= column5_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row1_top and row <= row1_bottom and column >= column6_left and column <= column6_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row1_top and row <= row1_bottom and column >= column7_left and column <= column7_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row1_top and row <= row1_bottom and column >= column8_left and column <= column8_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row1_top and row <= row1_bottom and column >= column9_left and column <= column9_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row1_top and row <= row1_bottom and column >= column10_left and column <= column10_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row1_top and row <= row1_bottom and column >= column11_left and column <= column11_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row1_top and row <= row1_bottom and column >= column12_left and column <= column12_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row1_top and row <= row1_bottom and column >= column13_left and column <= column13_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row1_top and row <= row1_bottom and column >= column14_left and column <= column14_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
+        if disp_ena = '1' then            -- Paddle position based on encoder_value
+            paddle_posL := encoder_value - paddle_width / 2;
+            paddle_posR := encoder_value + paddle_width / 2;
 
-        -- Second row blocks
-        elsif row >= row2_top and row <= row2_bottom and column >= column1_left and column <= column1_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row2_top and row <= row2_bottom and column >= column2_left and column <= column2_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row2_top and row <= row2_bottom and column >= column3_left and column <= column3_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row2_top and row <= row2_bottom and column >= column4_left and column <= column4_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row2_top and row <= row2_bottom and column >= column5_left and column <= column5_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row2_top and row <= row2_bottom and column >= column6_left and column <= column6_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row2_top and row <= row2_bottom and column >= column7_left and column <= column7_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row2_top and row <= row2_bottom and column >= column8_left and column <= column8_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row2_top and row <= row2_bottom and column >= column9_left and column <= column9_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row2_top and row <= row2_bottom and column >= column10_left and column <= column10_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row2_top and row <= row2_bottom and column >= column11_left and column <= column11_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row2_top and row <= row2_bottom and column >= column12_left and column <= column12_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row2_top and row <= row2_bottom and column >= column13_left and column <= column13_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
-        elsif row >= row2_top and row <= row2_bottom and column >= column14_left and column <= column14_right then
-            red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
+            -- Paddle coloring (White)
+            if row >= paddle_top and row <= paddle_bottom and column >= paddle_posL  and column <= paddle_posR then
+                red   <= X"FF";
+                green <= X"FF";
+                blue  <= X"FF";  
+            -- Border coloring (White)
+            elsif row <= BORDER_TOP or column <= BORDER_LEFT or column >= BORDER_RIGHT then
+                red   <= X"FF";
+                green <= X"FF";
+                blue  <= X"FF";
+            else 
+
+                -- Loop over rows and columns
+                for row_idx in 0 to 7 loop
+                    for col_idx in 0 to 13 loop
+                        if row >= row_tops(row_idx) and row <= row_bottoms(row_idx) and
+                           column >= column_lefts(col_idx) and column <= column_rights(col_idx) then
+                                red <= X"FF"; green <= X"FF"; blue <= X"FF";  -- Bright white
+                            end if;
+                        end if;
+                    end loop;
+                end loop;
+            end if;
         end if;
-		      -- Border coloring
-        if column <= BORDER_LEFT or column >= BORDER_RIGHT then
-            red   <= X"EF";
-            green <= X"EF";
-            blue  <= X"EF";
-			end if;
-        if row <= BORDER_TOP then
-          red   <= X"EF";
-          green <= X"EF";
-          blue  <= X"EF";
-			end if;
-
-		
     end process;
-
 end behavior;
