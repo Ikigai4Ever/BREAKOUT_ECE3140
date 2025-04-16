799084d86ed3cf66354c7d1cbea84e433df0fbc9
Ty <ty.h.ahrens@gmail.com>
2025-04-16 12:01:32 -0500

    made ball changes

--- Image_Generator.vhd
+++ Image_Generator.vhd
@@ -136,9 +136,9 @@ begin
     begin
         if disp_ena = '1' and rising_edge(delay_done) then
             if SW1 = '0' then 
-                ball_top_range;
-                ball_left_range;
-                quad1 <= '1';
+                ball_top_range <= ball_top_range;
+                ball_left_range <= ball_left_range;
+                quad3 <= '1';
             else 
                 if quad1 = '1' then
                     ball_left_range <= ball_left_range + 1;
